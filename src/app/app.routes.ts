6a79ead54ec492d65817d05431de70d9437d74f6
Darpeex <foxyyysp@mail.ru>
2024-09-05 10:42:41 +0300

    feat: добавлены роуты

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './pages/shop/shop.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
   {
     path: '', // Путь
     component: ShopComponent, // Компонент
-    pathMatch: 'full', // Параметр для главной страницы, чтобы при обращении к другим страницам он не открывался
+    pathMatch: 'full', // Параметр, чтобы при других страницах главная стр. не открывалась, т.к. 'feedback' тоже содержит в себе
+    // пустую строчку и также в браузере может открыться. 'full' же говорит, открываться, только в отсутствии других страниц
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent,
+  },
+  {
+    path: 'product/:id',
+    component: ProductComponent,
   },
 ];
